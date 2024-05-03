eadf33a7448aca27d7ca173c02bbb0eeea64386a
Ilyas Atabaev <ilyasatabaev731@gmail.com>
2024-05-03 21:09:50 +0300

    add kassa

---
+++ kassa/js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $(function () {
+        $('.show_second_cont').click(function(){
+            $('.main_cont1').css('display', 'none');
+            $('.main_cont2').css('display','block');
+        });
+
+        $('.show_first_cont').click(function(){
+            $('.main_cont1').css('display', 'block');
+            $('.main_cont2').css('display','none');
+        });
+
+        $('#skipButton').click(function(){
+            window.location.href = "https://ilyasatabaev731.github.io/";
+        })
+
+        $('#workButton').click(function(){
+            window.location.href = "https://ilyasatabaev731.github.io/";
+        })
+    });
+
+    $(function () {
+        new WOW().init();
+    });
+
+    $(function(){
+        $(".main_cont1").on("swipeleft", swipeHandler);
+        $(".main_cont1").on("swiperight", swipeHandler);
+        $(".main_cont2").on("swipeleft", swipeHandler2);
+        $(".main_cont2").on("swiperight", swipeHandler2);
+      
+        function swipeHandler(event) {
+        //   $(event.target).removeClass("right left");
+          $('.main_cont1').css('display', 'none');
+          $('.main_cont2').css('display','block');
+          event.type == 'swipeleft' ? $(event.target).addClass("left") : $(event.target).addClass("right");
+        }
+
+        function swipeHandler2(event) {
+            //   $(event.target).removeClass("right left");
+              $('.main_cont1').css('display', 'block');
+              $('.main_cont2').css('display','none');
+              event.type == 'swipeleft' ? $(event.target).addClass("left") : $(event.target).addClass("right");
+        }
+    })
+
+});
+
+
