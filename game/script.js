d5a64af96112d5f268a69cbb1ae186aee3bc1b5c
IlyasAtabaev731 <pulpasaur731@gmail.com>
2024-05-18 03:12:06 +0300

    fix

--- game/script.js
+++ game/script.js
@@ -1,7 +1,7 @@
 import './node_modules/axios/dist/axios.js';
 
 
-const defaultUrl = "https://feline-immense-lionfish.ngrok-free.app";
+const defaultUrl = "https://dassie-golden-bull.ngrok-free.app";
 
 async function fetchData(endpoint, data) {
   try {
